d88f627ec3166b4787addf57855e72e24dbb0053
Bohdan <bkryzhniy@gmail.com>
2020-10-12 10:45:31 +0300

    style: change amount displayed movies

--- src/elements/MainPage.js
+++ src/elements/MainPage.js
@@ -4,29 +4,25 @@ import React, { useEffect } from 'react';
 const MainPage = (props) => {
 
     useEffect(() => {
-        fetch("http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c")
-            .then(res => res.json())
-            .then(res => props.setFilms(res.results))
-            .catch(err => console.error(err))
+        if (props.films.length === 0) {
+            fetch(`http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&page=4`)
+                .then(res => res.json())
+                .then(res => props.setFilms(res.results))
+                .catch(err => console.error(err))
+        }
     }, [])
 
-    const postersList = props.films
-        .map((film, i) => {
-            if (i < 12) {
-                return <div key={film.id} className="col-6 col-md-3 col-lg-2">
-                    <div className='poster-wrap'>
-                        <div className="container-fluid">
-                            <div className="row">
-                                <div className="img-wrap">
-                                   <img src={`http://image.tmdb.org/t/p/w342/${film.poster_path}`} className="img-responsive" />
+    // for pagination
+    // &page=4
 
-                                    <div className='poster-text'><div><strong>{film.original_title}</strong></div></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    const postersList = props.films
+        .map(film => {
+            return <div key={film.id} className='poster-wrap'>
+                <div className='img-wrap'>
+                    <img src={`http://image.tmdb.org/t/p/w342/${film.poster_path}`} alt='img...' className='' />
+                    <div className='poster-text'><div><strong>{film.original_title}</strong></div></div>
                 </div>
-            }
+            </div>
         })
 
     console.log(props.films)
@@ -37,10 +33,8 @@ const MainPage = (props) => {
                 <div className='main-head'>
                     <h3>Latest releases</h3>
                 </div>
-                <div className='main-content container'>
-                    <div className='row'>
-                        {postersList}
-                    </div>
+                <div className='main-content'>
+                    {postersList}
                 </div>
                 <div className='main-footer'>
                     <h3>pagination</h3>
