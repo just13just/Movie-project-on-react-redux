0bf5ea2b43b02a5a431cb9a077d06ee016da1cbc
Bohdan <bkryzhniy@gmail.com>
2020-10-14 01:48:22 +0300

    feat: made favorite page markup

--- src/elements/FavoritesPage.js
+++ src/elements/FavoritesPage.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
-const FavoritesPage = () => {
-    return <h1>Favorites Page</h1>
+const FavoritesPage = (props) => {
+
+    const postersList = props.films
+        .map(film => {
+            return <div key={film.id} className='favorites-grid-container'>
+                <div className='favorites-img-container'>
+                    <img src={`http://image.tmdb.org/t/p/w342/${film.poster_path}`} alt='img...' className='' />
+                </div>
+                <div className='favorites-content-container'>
+                    <div className='favorites-text-container'>
+                        <h3><strong>{film.original_title}</strong></h3>
+                        <button type="button" className="btn btn-outline-secondary"><strong>Unfavorite</strong></button>
+                    </div>
+                    <div className='favorites-overview-wrap'>
+                        <div className='favorites-overview'>{film.overview}</div>
+                    </div>
+                </div>
+            </div>
+        })
+
+
+
+    return <div className='favorites-page-wrap'>
+        <div className='favorite-page'>
+            <div className='favorite-head'>
+                <h3>My favorite</h3>
+            </div>
+            {postersList}
+        </div>
+    </div>
 }
 
 export default FavoritesPage;
--- src/elements/FavoritesPageContainer copy.js
+++ src/elements/FavoritesPageContainer copy.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import { connect } from 'react-redux';
+import { setFilmsAC } from '../redux/filmsReducer';
 import FavoritesPage from './FavoritesPage';
 
-const FavoritesPageContainer = () => {
-    return <FavoritesPage />
+const mapStateToProps = (state) => {
+    return {
+        films: state.mainData.films
+    }
 }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setFilms: (films) => {
+            dispatch(setFilmsAC(films))
+        }
+    }
+}
+
+const FavoritesPageContainer = connect(mapStateToProps, mapDispatchToProps)(FavoritesPage)
 
 export default FavoritesPageContainer;
