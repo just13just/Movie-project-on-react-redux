d31c41a9016060d9eba71691183d43cfa579611e
Bohdan <bkryzhniy@gmail.com>
2020-10-28 16:42:40 +0200

    refactor: contain totalCount and pageNum variables to redux store

--- src/elements/MainPageContainer.js
+++ src/elements/MainPageContainer.js
@@ -1,13 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { setFilmsAC, setModalFilmNumAC } from '../redux/filmsReducer';
+import {
+    setFilmsAC,
+    setModalFilmNumAC,
+    setPageNumAC,
+    setTotalCountAC
+} from '../redux/filmsReducer';
 import MainPage from './MainPage';
 
 const MainPageContainer = (props) => {
 
-    const { films, setFilms, modalFilmNum, setModalFilmNum, toggle, setToggle } = props;
-    const [pageNum, setPageNum] = useState(null);
-    const [totalCount, setTotalCount] = useState(1)
+    const {
+        films,
+        setFilms,
+        modalFilmNum,
+        setModalFilmNum,
+        toggle,
+        setToggle,
+        totalCount,
+        setTotalCount,
+        pageNum,
+        setPageNum
+    } = props;
 
     useEffect(() => {
         fetch(`http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&page=${pageNum}`)
@@ -18,7 +32,7 @@ const MainPageContainer = (props) => {
                 setTotalCount(res.total_results)
             })
             .catch(err => console.error(err))
-    }, [pageNum, setFilms])
+    }, [pageNum, setFilms, setTotalCount, setPageNum])
 
     return (
         <MainPage
@@ -37,6 +51,8 @@ const MainPageContainer = (props) => {
 const mapStateToProps = (state) => {
     return {
         films: state.mainData.films,
+        totalCount: state.mainData.totalCount,
+        pageNum: state.mainData.pageNum,
         modalFilmNum: state.mainData.modalFilmNum
     }
 }
@@ -44,6 +60,12 @@ const mapDispatchToProps = (dispatch) => {
     return {
         setFilms: (films) => {
             dispatch(setFilmsAC(films))
+        },
+        setTotalCount: (count) => {
+            dispatch(setTotalCountAC(count))
+        },
+        setPageNum: (num) => {
+            dispatch(setPageNumAC(num))
         },
         setModalFilmNum: (films) => {
             dispatch(setModalFilmNumAC(films))
--- src/redux/filmsReducer.js
+++ src/redux/filmsReducer.js
@@ -1,8 +1,12 @@
 const SET_FILMS = "SET_FILMS";
+const SET_TOTAL_COUNT = "SET_TOTAL_COUNT";
+const SET_PAGE_NUM = "SET_PAGE_NUM";
 const SET_MODAL_FILM_NUM = "SET_MODAL_FILM_NUM";
 
 const initialState = {
     films: [],
+    totalCount: 0,
+    pageNum: null,
     modalFilmNum: null
 }
 
@@ -11,6 +15,12 @@ const filmsReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_FILMS:
             return { ...state, films: [...action.films] }
+
+        case SET_TOTAL_COUNT:
+            return { ...state, totalCount: action.count }
+
+        case SET_PAGE_NUM:
+            return { ...state, pageNum: action.num }
 
         case SET_MODAL_FILM_NUM:
             return { ...state, modalFilmNum: action.num }
@@ -22,6 +32,12 @@ const filmsReducer = (state = initialState, action) => {
 
 export const setFilmsAC = (films) => {
     return { type: SET_FILMS, films }
+}
+export const setTotalCountAC = (count) => {
+    return { type: SET_TOTAL_COUNT, count }
+}
+export const setPageNumAC = (num) => {
+    return { type: SET_PAGE_NUM, num }
 }
 export const setModalFilmNumAC = (num) => {
     return { type: SET_MODAL_FILM_NUM, num }
